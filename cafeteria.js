f30501a6f9814f85105d897f1991195a6beaee68
thaisa <limathaisa1204@gmail.com>
2025-09-04 17:35:43 -0300

    Add files via upload

---
+++ cafeteria.js
@@ -0,0 +1,37 @@
+const produtos = [ {
+    nome: "Café Expresso",
+    preco: 8,
+    img: ""
+}
+
+{
+    nome: "cappuccino",
+    preco: 12 ,
+    img: ""
+}
+{
+    nome: "Suco de Laranja",
+    preco: 10,
+    img: ""
+}
+{
+    nome: "Agua Mineral",
+    preco: 6,
+    img: ""
+}
+{
+    nome: "Sanduiche Natural",
+    preco: 
+    img: ""
+}
+{
+    nome: "Fatia de Bolo",
+    preco: 8,
+    img: ""
+}
+{
+    nome: "Fatia de Bolo",
+    preco: 8,
+    img: ""
+}
+]
